a907445cd93190f700675d333e7b8a8b2c6e0744
CorpoWolf <gmangavin1@gmail.com>
2024-03-21 10:15:48 -0700

    Added DX11 initialization

--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #define _UNICODE
 
 #include <windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "D3DCompiler.lib")
 
 // Function declaration for the Window Procedure
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+ID3D11Device* g_pd3dDevice = NULL;
+ID3D11DeviceContext* g_pImmediateContext = NULL;
+IDXGISwapChain* g_pSwapChain = NULL;
+ID3D11RenderTargetView* g_pRenderTargetView = NULL;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
 	const wchar_t CLASS_NAME[] = L"Sample Window Class";
@@ -39,6 +47,48 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 
 	ShowWindow(hwnd, nCmdShow);
 
+	DXGI_SWAP_CHAIN_DESC sd = {0};
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = 800; // Use actual window size
+	sd.BufferDesc.Height = 600; // Use actual window size
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hwnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+
+	D3D_FEATURE_LEVEL FeatureLevelsRequested = D3D_FEATURE_LEVEL_11_0;
+	UINT numFeatureLevelsRequested = 1;
+	D3D_FEATURE_LEVEL FeatureLevelsSupported;
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain( NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &FeatureLevelsRequested, numFeatureLevelsRequested,
+												D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &FeatureLevelsSupported, &g_pImmediateContext );
+	if (FAILED(hr))
+	{
+		return 1;
+	}
+
+		// Create a render target view
+	ID3D11Texture2D* pBackBuffer = NULL;
+	g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_pRenderTargetView);
+	pBackBuffer->Release();
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, NULL);
+
+	// Setup the viewport
+	D3D11_VIEWPORT vp;
+	vp.Width = 800; // Use actual window size
+	vp.Height = 600; // Use actual window size
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	g_pImmediateContext->RSSetViewports(1, &vp);
+
 	// Run the message loop.
 	MSG msg = { };
 	while (GetMessage(&msg, NULL, 0, 0)) {
@@ -56,13 +106,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 			return 0;
 
 		case WM_PAINT: {
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hwnd, &ps);
+			// Clear the back buffer
+			float ClearColor[4] = {0.25f, 0.25f, 0.25f, 1.0f}; // RGBA
+			g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
 
-			// All painting occurs here, between BeginPaint and EndPaint.
-			FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-			EndPaint(hwnd, &ps);
+			// Present the back buffer to the screen
+			g_pSwapChain->Present(0, 0);
 		}
 		return 0;
 	}
